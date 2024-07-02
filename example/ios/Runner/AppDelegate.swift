bcf3eacd64acc201a4c38fee11a376f74332e61d
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-07-02 21:09:25 +0530

    feat: iOS implementation

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,13 +1,20 @@
 import Flutter
 import UIKit
+import restart_app
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // --------------------------------------------------
+        RestartAppPlugin.generatedPluginRegistrantRegisterCallback = { [weak self] in
+            GeneratedPluginRegistrant.register(with: self!)
+        }
+        // --------------------------------------------------
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
 }
--- ios/Classes/RestartAppPlugin.swift
+++ ios/Classes/RestartAppPlugin.swift
@@ -2,18 +2,29 @@ import Flutter
 import UIKit
 
 public class RestartAppPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "restart_app", binaryMessenger: registrar.messenger())
-    let instance = RestartAppPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static var generatedPluginRegistrantRegisterCallback: () -> Void = {
+        NSLog("WARNING: generatedPluginRegistrantRegisterCallback is not assigned by the AppDelegate.")
     }
-  }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "in.farmako/restart_app", binaryMessenger: registrar.messenger())
+        let instance = RestartAppPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "restart":
+            let flutterViewController = UIApplication.shared.keyWindow?.rootViewController as? FlutterViewController
+            UIApplication.shared.keyWindow?.rootViewController = FlutterViewController(
+                project: nil,
+                nibName: nil,
+                bundle: nil
+            )
+            RestartAppPlugin.generatedPluginRegistrantRegisterCallback()
+            result(true)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
